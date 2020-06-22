6aa1f3727aca7c0b562d945353561c5ac97fa5fc
Rostyk <rostikbanderstadt@gmail.com>
2020-06-22 17:38:17 +0300

    2

---
+++ test/pravda-perekupa/js/main.js
@@ -0,0 +1,6 @@
+$(function () {
+	var austDay = new Date();
+	austDay = new Date(austDay.getFullYear() + 1, 1 - 1, 26);
+	$('#defaultCountdown').countdown({until: austDay});
+	$('#year').text(austDay.getFullYear());
+});
