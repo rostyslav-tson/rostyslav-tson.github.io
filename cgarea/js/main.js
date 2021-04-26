e9bff14173e3ec2c2e79c8f36c7cdb3544a024d0
Rostyk <rostikbanderstadt@gmail.com>
2021-04-26 18:16:32 +0300

    1

---
+++ cgarea/js/main.js
@@ -0,0 +1,48 @@
+  function myFunctionOff() {
+    document.getElementById("demo").innerHTML = "3D Модели"
+  }
+  function myFunction1() {
+    document.getElementById("demo").innerHTML = "<img src='img/icons_menu/Архитектура.svg'>"
+  }
+  function myFunction2() {
+    document.getElementById("demo").innerHTML = "<img src='img/icons_menu/Декорация.svg'>"
+  }
+  function myFunction3() {
+    document.getElementById("demo").innerHTML = "<img src='img/icons_menu/Детская.svg'>"
+  }
+  function myFunction4() {
+    document.getElementById("demo").innerHTML = "<img src='img/icons_menu/Кухня.svg'>"
+  }
+  function myFunction5() {
+    document.getElementById("demo").innerHTML = "<img src='img/icons_menu/Мебель.svg'>"
+  }
+  function myFunction6() {
+    document.getElementById("demo").innerHTML = "<img src='img/icons_menu/Одежда.svg'>"
+  }
+  function myFunction7() {
+    document.getElementById("demo").innerHTML = "<img src='img/icons_menu/Освещение.svg'>"
+  }
+  function myFunction8() {
+    document.getElementById("demo").innerHTML = "<img src='img/icons_menu/Санузел.svg'>"
+  }
+  function myFunction9() {
+    document.getElementById("demo").innerHTML = "<img src='img/icons_menu/Ландшафт.svg'>"
+  }
+  function myFunction10() {
+    document.getElementById("demo").innerHTML = "<img src='img/icons_menu/Транспорт.svg'>"
+  }
+  function myFunction11() {
+    document.getElementById("demo").innerHTML = "<img src='img/icons_menu/Персонаж.svg'>"
+  }
+  function myFunction12() {
+    document.getElementById("demo").innerHTML = "<img src='img/icons_menu/Животные.svg'>"
+  }
+  function myFunction13() {
+    document.getElementById("demo").innerHTML = "<img src='img/icons_menu/Техника.svg'>"
+  }
+  function myFunction14() {
+    document.getElementById("demo").innerHTML = "<img src='img/icons_menu/Разные.svg'>"
+  }
+  function myFunction15() {
+    document.getElementById("demo").innerHTML = "<img src='img/icons_menu/Скрипт.svg'>"
+  }
